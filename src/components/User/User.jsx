f708b79af4818d52800856b38c4abcdc02ae0ded
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-26 15:04:08 -0700

    Implement user edit

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import styles from './user.module.css';
+import { useNavigate } from 'react-router-dom';
 
 function User(props) {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({ username: '' });
+  const [errors, setErrors] = useState(null);
+  const navigate = useNavigate();
   const { username } = props;
+
   useEffect(() => {
     const token = Cookies.get('jwt_token');
     async function getUser() {
@@ -24,10 +28,60 @@ function User(props) {
     getUser();
   }, []);
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...User, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = Cookies.get('jwt_token');
+
+    try {
+      const response = await fetch('http://localhost:3000/users/' + username, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token
+        },
+        body: JSON.stringify(user)
+      });
+      const data = await response.json();
+      if (response.ok) {
+        //navigate to new username page
+        navigate('/' + data.username);
+        return;
+      } else {
+        setErrors(data.errors);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div>
-      <p>user</p>
-      <p>{user.username}</p>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="username">Edit username</label>
+
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={user.username}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit"> Submit</button>
+      </form>
+      {errors && (
+        <div>
+          {errors.map((error) => (
+            <h4 key={error.path}>{error.msg}</h4>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
