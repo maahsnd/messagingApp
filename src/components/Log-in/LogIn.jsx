e9ff3de6f03f59abee3a52db4f204e1c67237cad
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-26 10:38:58 -0700

    Implement log in component Untested.

---
+++ src/components/Log-in/LogIn.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
+import styles from './log-in.module.css';
+import { useNavigate } from 'react-router-dom';
+
+function LogIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/log-in', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+      if (response.ok) {
+        const data = await response.json();
+        // Store the JWT token in cookies
+        Cookies.set('jwt_token', data.token);
+        navigate('/');
+        return;
+      } else {
+        // Handle authentication error
+        console.error('Authentication failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className={styles.loginForm}>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default LogIn;
