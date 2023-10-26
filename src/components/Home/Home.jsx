bfdf29fc55db98e44a046370656fd351ce01c328
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-26 10:56:22 -0700

    Implement home Return dummy content if logged in (implement later). Else serve links to log in or sign up.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
+import styles from './log-in.module.css';
+import { Link } from 'react-router-dom';
+import LogIn from '../Log-in/LogIn';
+import SignUp from '../Sign-up/SignUp';
 
 function Home() {
-  return <p>log in</p>;
+  const [authToken, setAuthToken] = useState(null);
+
+  useEffect(() => {
+    const token = Cookies.get('jwt_token');
+    if (token) {
+      setAuthToken(token);
+    }
+    return;
+  }, []);
+
+  if (authToken)
+    return (
+      <div>
+        <p>messages</p>
+      </div>
+    );
+  else {
+    return (
+      <div>
+        <Link to="/log-in">Log in</Link>
+        <p>or</p>
+        <Link to="/sign-up">Sign up</Link>
+      </div>
+    );
+  }
 }
+export default Home;
