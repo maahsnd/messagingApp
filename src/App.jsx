20f153b553ae0633f9a45e78f14699ae80fa45d2
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-26 10:38:37 -0700

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from '../src/components/Home/Home';
+import LogIn from '../src/components/Log-in/LogIn';
+import SignUp from '../src/components/Sign-up/SignUp';
 
 function App() {
-  const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+  const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/log-in', element: <LogIn /> },
+    { path: '/sign-up', element: <SignUp /> }
+  ]);
 
   return <RouterProvider router={router} />;
 }
