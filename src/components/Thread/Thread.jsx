8374ff76b93806e169292cfaf41e4b18fcb0d229
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-27 14:48:52 -0700

    Create recipient select

--- src/components/Thread/Thread.jsx
+++ src/components/Thread/Thread.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import styles from './thread.module.css';
 
-function Thread({ selectedthread }) {
+function Thread({ selectedThread, newThread }) {
   return (
     <div className={styles.thread}>
-      {selectedthread ? (
+      {selectedThread && 
         <div className={styles.messages}>
           {/* Display messages for the selected thread */}
         </div>
-      ) : (
-        <div className={styles.placeholder}>
-          Select a thread or create a new thread.
-        </div>
-      )}
+       }
+      {newThread && 
+      <div className={styles.threadForm}>
+        <form >
+          <div className={styles.contactField}>
+            <label htmlFor="contactSelect">Select recipient/s</label>
+            <select name="contactSelect" id="contactSelect">
+              {newThread.map((user) => (
+                <option value={user.username} key={user.username}>{user.username}</option>
+              ))}
+            </select>
+          </div>
+        </form>
+      </div>}
     </div>
   );
 }
