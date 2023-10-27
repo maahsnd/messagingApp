a2465c642785bb73f4fc4398ed13fa27969784da
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-27 13:59:12 -0700

    Create sidebar skeleton

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './sidebar.module.css';
+
+function Sidebar({ onNewThreadClick, conversations, onConversationClick }) {
+  console.log(conversations);
+  return (
+    <div className={styles.sidebar}>
+      <button onClick={onNewThreadClick}>Create New Thread</button>
+      {conversations.map((conversation) => (
+        <button
+          key={conversation.id}
+          onClick={() => onConversationClick(conversation)}
+        >
+          {conversation.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
