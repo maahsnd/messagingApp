df2eed7f844e5350d8bafcc39b44a419831f0bb9
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-27 13:57:01 -0700

    Change user route Separate from home.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from '../src/components/Home/Home';
 import LogIn from '../src/components/Log-in/LogIn';
 import SignUp from '../src/components/Sign-up/SignUp';
+import User from './components/User/User';
 
 function App() {
   const router = createBrowserRouter([
     { path: '/log-in', element: <LogIn /> },
     { path: '/sign-up', element: <SignUp /> },
     { path: '/:username', element: <Home /> },
+    { path: '/:username/edit', element: <User /> },
     { path: '/', element: <Home /> }
   ]);
 
