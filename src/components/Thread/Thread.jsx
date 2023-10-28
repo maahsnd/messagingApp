a257aa2eb436fb04e3b2f119dd5b71747303a223
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-28 12:15:51 -0700

    Successfully send new thread

--- src/components/Thread/Thread.jsx
+++ src/components/Thread/Thread.jsx
@@ -1,17 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './thread.module.css';
 import Cookies from 'js-cookie';
 import { useParams } from 'react-router-dom';
 
-function Thread({  newThreadForm, selectedThread }) {
+function Thread({ newThreadForm, selectedThread }) {
   const [recipients, setRecipients] = useState([]);
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('');
   const [contacts, setContacts] = useState([]);
-  const {username} = useParams()
+  const { username } = useParams();
 
   //fetch contacts
-  useEffect(()=> {
-    const fetchContacts = async() => {
+  useEffect(() => {
+    const fetchContacts = async () => {
       const token = Cookies.get('jwt_token');
       const response = await fetch(`http://localhost:3000/users/`, {
         method: 'GET',
@@ -24,84 +24,112 @@ function Thread({  newThreadForm, selectedThread }) {
         console.error('Error fetching contacts');
       }
       const data = await response.json();
-      setContacts(data);}
-    fetchContacts()
-  },[])
+      setContacts(data);
+    };
+    fetchContacts();
+  }, []);
 
   //submit new thread & message
   const submitNewThread = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const token = Cookies.get('jwt_token');
-    const userId = Cookies.get('userId')
-    const recipientIds = recipients.map((recipient) => recipient = recipient._id)
+    const userId = Cookies.get('user_id');
+    const recipientIds = recipients.map(
+      (recipient) => (recipient = recipient._id)
+    );
     const body = {
       text: message,
       from: userId,
       to: recipientIds
-    }
-    console.log(body)
-    const response = await fetch(`http://localhost:3000/${username}/threads/messages`, {
+    };
+    const response = await fetch(
+      `http://localhost:3000/users/${username}/threads`,
+      {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           Authorization: 'Bearer ' + token
         },
         body: JSON.stringify(body)
-      });
-      if (!response.ok) {
-        console.error('Error sending new message');
       }
-      const data = await response.json();
-  }
+    );
+    if (!response.ok) {
+      console.error('Error sending new message');
+    }
+    const data = await response.json();
+    console.log(data);
+  };
 
   const selectRecipient = (e) => {
-    e.preventDefault()
-    setRecipients([...recipients, e.target.value])
-  } 
-  const messageChange = () => {
-    setMessage(e.target.value)
-  }
+    const selectedRecipient = contacts.find(
+      (contact) => contact.username === e.target.value
+    );
+    setRecipients([...recipients, selectedRecipient]);
+  };
+  const messageChange = (e) => {
+    setMessage(e.target.value);
+  };
   const removeRecipient = (e) => {
-    const filtered = recipients.filter((recipient) => recipient != e.target.value);
+    const filtered = recipients.filter(
+      (recipient) => recipient.username != e.target.value
+    );
     setRecipients(filtered);
-  }
+  };
   return (
     /* SELECTED THREAD*/
     <div className={styles.thread}>
-      {selectedThread && 
+      {selectedThread && (
         <div className={styles.messages}>
           {/* Display messages for the selected thread */}
         </div>
-       }
-    {/* NEW THREAD*/}
-      {newThreadForm && 
-      <div className={styles.threadForm}>
-        <form onSubmit={submitNewThread}>
-          <div className={styles.contactField}>
-            <label htmlFor="contactSelect">Select recipient/s</label>
-        <select name="contactSelect" id="contactSelect" onChange={selectRecipient}>
-              {contacts.map((user) => (
-                <option value={user.username} key={user.username}>{user.username}</option>
-              ))}
-            </select> 
-          </div>
-          <div className={styles.recipientsContainer}>
-            <h4>Recipients:</h4>
-                {recipients.map((recipient) => (
-                  <div className={styles.recipient} key={recipient}>
-                    <p>{recipient}</p>
-                    <button value={recipient} onClick={removeRecipient}>x</button>
-                    </div>
+      )}
+      {/* NEW THREAD*/}
+      {newThreadForm && (
+        <div className={styles.threadForm}>
+          <form onSubmit={submitNewThread}>
+            <div className={styles.contactField}>
+              <select
+                name="contactSelect"
+                id="contactSelect"
+                onChange={selectRecipient}
+              >
+                <option selected disabled value=" ">
+                  Select a recipient
+                </option>
+                {contacts.map((user) => (
+                  <option value={user.username} key={user.username}>
+                    {user.username}
+                  </option>
                 ))}
-          </div>
-          <div className={styles.messageField}>
-            <textarea value={message} onChange={messageChange} name="messageText" id="messageText" cols="30" rows="10"></textarea>
-          </div>
-          <div className={styles.submitButton}>
-                  <button type='submit'>Send</button>
-          </div>
-        </form>
-      </div>}
+              </select>
+            </div>
+            <div className={styles.recipientsContainer}>
+              <h4>Recipients:</h4>
+              {recipients.map((recipient) => (
+                <div className={styles.recipient} key={recipient.username}>
+                  <p>{recipient.username}</p>
+                  <button value={recipient.username} onClick={removeRecipient}>
+                    x
+                  </button>
+                </div>
+              ))}
+            </div>
+            <div className={styles.messageField}>
+              <textarea
+                value={message}
+                onChange={messageChange}
+                name="messageText"
+                id="messageText"
+                cols="30"
+                rows="10"
+              ></textarea>
+            </div>
+            <div className={styles.submitButton}>
+              <button type="submit">Send</button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
