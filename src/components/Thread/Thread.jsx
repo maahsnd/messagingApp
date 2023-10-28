7045b4194d3dfe0b23ea2c7ca609cedd95a15484
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-28 10:34:10 -0700

    Add controlled text area

--- src/components/Thread/Thread.jsx
+++ src/components/Thread/Thread.jsx
@@ -3,21 +3,27 @@ import styles from './thread.module.css';
 
 function Thread({ newThreadContacts, newThreadForm, selectedThread }) {
   const [recipients, setRecipients] = useState([]);
+  const [message, setMessage] = useState(null)
   const selectRecipient = (e) => {
     e.preventDefault()
     setRecipients([...recipients, e.target.value])
   } 
+  const messageChange = () => {
+    setMessage(e.target.value)
+  }
   const removeRecipient = (e) => {
     const filtered = recipients.filter((recipient) => recipient != e.target.value);
     setRecipients(filtered);
   }
   return (
+    /* SELECTED THREAD*/
     <div className={styles.thread}>
       {selectedThread && 
         <div className={styles.messages}>
           {/* Display messages for the selected thread */}
         </div>
        }
+    {/* NEW THREAD*/}
       {newThreadForm && 
       <div className={styles.threadForm}>
         <form >
@@ -37,6 +43,12 @@ function Thread({ newThreadContacts, newThreadForm, selectedThread }) {
                     <button value={recipient} onClick={removeRecipient}>x</button>
                     </div>
                 ))}
+          </div>
+          <div className={styles.messageField}>
+            <textarea value={message} onChange={messageChange} name="messageText" id="messageText" cols="30" rows="10"></textarea>
+          </div>
+          <div className={styles.submitButton}>
+                  <button>Send</button>
           </div>
         </form>
       </div>}
