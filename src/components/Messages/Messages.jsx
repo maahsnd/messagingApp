74d0b0f1b8d40f0a1e5fda78726a5fd5cb602e13
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-28 09:48:07 -0700

    Comment

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,6 +3,7 @@ import styles from './Messages.module.css'; // Import CSS Module
 import Sidebar from '../Sidebar/Sidebar';
 import Thread from '../Thread/Thread';
 
+//messages renders and contains sidebar and thread
 function Messages() {
   const [selectedConversation, setSelectedConversation] = useState(null);
   const conversations = ['hey', 'hi', 'hoo'];
