c24778b978d1f98ed9137f30a7f726b4d98313fc
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-28 13:29:54 -0800

    Limit contact display, use ellipse for extras

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,9 +16,19 @@ function Sidebar({ handleNewThreadClick, threads, onThreadClick }) {
           onClick={() => onThreadClick(thread)}
         >
           {thread.users.map((user, index) => {
+            //display ellipse in place of 4th user
+            if (index === 3) {
+              return ' & ...'
+            }
+            //do not display beyond ellipse
+            if (index > 3) {
+              return
+            }
+            //do not display users own name
             if (user.username === username) {
               return
             }
+            //add preceding comma if not users own name or first name
             if (index != 0 && thread.users[index-1].username != username) {
               return ', ' + user.username;
             }
